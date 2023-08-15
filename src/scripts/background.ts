fa00b6b40ef98995e12dae688f141f8435221da2
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-08-15 14:05:03 +0300

    Changed defaults

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -9,46 +9,48 @@ export type Shortcuts = Record<string, Shortcut>
 const DEFAULT_SHORTCUTS: Shortcuts = {
 	like: {
 		label: "Like",
-		keys: "ALT+ENTER",
+		keys: "ALT+Z",
 		clickQuerySelector: `
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button a:not([aria-pressed="true"]), /* Shorts Classic */
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button button:not([aria-pressed="true"]), /* Shorts New Layout */
-      ytd-watch-metadata #top-level-buttons-computed #segmented-like-button button:not([aria-pressed="true"]), /* New Layout */
-      ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(1n) button:not([aria-pressed="true"]) /* Classic Layout */
-    `,
+     			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button a:not([aria-pressed="true"]), /* Shorts Classic */
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button button:not([aria-pressed="true"]), /* Shorts New Layout */
+      			ytd-watch-metadata #top-level-buttons-computed #segmented-like-button button:not([aria-pressed="true"]), /* New Layout */
+      			ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(1n) button:not([aria-pressed="true"]) /* Classic Layout */
+    		`,
 		sortIndex: 0.01,
 	},
 	dislike: {
 		label: "Dislike",
-		keys: "ALT+BACKSPACE",
+		keys: "ALT+C",
 		clickQuerySelector: `
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button a:not([aria-pressed="true"]), /* Shorts */
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button button:not([aria-pressed="true"]), /* Shorts New Layout */
-      ytd-watch-metadata #top-level-buttons-computed #segmented-dislike-button button:not([aria-pressed="true"]), /* New Layout */
-      ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(2n) button:not([aria-pressed="true"]) /* Classic Layout */
-    `,
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button a:not([aria-pressed="true"]), /* Shorts */
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button button:not([aria-pressed="true"]), /* Shorts New Layout */
+      			ytd-watch-metadata #top-level-buttons-computed #segmented-dislike-button button:not([aria-pressed="true"]), /* New Layout */
+      			ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(2n) button:not([aria-pressed="true"]) /* Classic Layout */
+    		`,
 		sortIndex: 0.02,
 	},
 	neutral: {
 		label: "Neutral",
-		keys: `ALT+0`,
+		keys: `ALT+X`,
 		clickQuerySelector: `
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button a[aria-pressed="true"], /* Shorts Like Classic */
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button a[aria-pressed="true"], /* Shorts Dislike Classic */
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button button[aria-pressed="true"], /* Shorts Like New Layout */
-      ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button button[aria-pressed="true"], /* Shorts Dislike New Layout */
-      ytd-watch-metadata #top-level-buttons-computed ytd-segmented-like-dislike-button-renderer button[aria-pressed="true"], /* New Layout Like/Dislike */
-      ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(1n) button[aria-pressed="true"], /* Classic Layout Like */
-      ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(2n) button[aria-pressed="true"] /* Classic Layout Dislike */
-    `,
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button a[aria-pressed="true"], /* Shorts Like Classic */
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button a[aria-pressed="true"], /* Shorts Dislike Classic */
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#like-button button[aria-pressed="true"], /* Shorts Like New Layout */
+      			ytd-reel-player-overlay-renderer ytd-toggle-button-renderer#dislike-button button[aria-pressed="true"], /* Shorts Dislike New Layout */
+      			ytd-watch-metadata #top-level-buttons-computed ytd-segmented-like-dislike-button-renderer button[aria-pressed="true"], /* New Layout Like/Dislike */
+      			ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(1n) button[aria-pressed="true"], /* Classic Layout Like */
+      			ytd-watch-metadata #top-level-buttons-computed > ytd-toggle-button-renderer:nth-of-type(2n) button[aria-pressed="true"] /* Classic Layout Dislike */
+    		`,
 		sortIndex: 0.03,
 	},
-	search: {
-		label: "Search",
+	subscribe: {
+		label: "Subscribe",
 		keys: "ALT+S",
-		clickQuerySelector: "#search-form input#search",
-		sortIndex: 0.04,
-	},
+		clickQuerySelector: `
+			#subscribe-button ytd-subscribe-button-renderer:not([subscribed])
+  		`,
+		sortIndex: 0.04
+	}
 }
 
 export type BackgroundMethod<Params extends any[], Returns> = (...params: Params) => Promise<Returns>
